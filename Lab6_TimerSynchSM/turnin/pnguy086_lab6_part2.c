47fb817965b0693e4a5982e4c7de85f79b6853e6
Paul Nguyen <paul061799@gmail.com>
2021-10-24 21:10:20 -0700

    Added Lab 6 Demo Links

--- Lab6_TimerSynchSM/turnin/pnguy086_lab6_part2.c
+++ Lab6_TimerSynchSM/turnin/pnguy086_lab6_part2.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ * Demo Link: https://youtu.be/q3QQhsZrImU
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
