f1cef686c100d704d07e9a18f86c4e5f742c8842
Paul Nguyen <paul061799@gmail.com>
2021-10-24 21:07:10 -0700

    added demo link l5p3

--- Lab5_atmega1284chip/turnin/pnguy086_lab5_part3.c
+++ Lab5_atmega1284chip/turnin/pnguy086_lab5_part3.c
@@ -6,7 +6,7 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
- *	Demo Link: 
+ *	Demo Link: https://youtu.be/ibp6xGga_go
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
